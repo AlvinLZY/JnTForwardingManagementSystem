9e3324a29dd6bdf335154be5e2d938ca4fb7c8de
AlvinLZY <86895049+AlvinLZY@users.noreply.github.com>
2022-04-02 00:10:55 +0800

    rename Project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>You Found Me HEHE</title>
+        <title>You Found Me</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
