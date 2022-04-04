1af7b3ac0128ba776cb16bb2a3fc08d18bfff2a2
LokeCK <choonkeaiii@gmail.com>
2022-04-04 22:32:53 +0800

    TestCommit 2

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,11 @@ Route::resource('master','MasterController');
 Route::get('/welcome','MasterController@welcome');
 Route::get('/viewSchedule','ScheduleController@index');
 
+Route::resource('order',OrderController::class);
+//Route::get('/order','OrderController@all');
+Route::patch('order/{orderID}','OrderController@update')->name('order.updaste');
+//Route::get('/OrderUpdate','OrderController@');
+//Route::patch("order/{orderID}", 'OrderController@update')->name('order.update');
+//Route::patch("order/edit/{orderID}", [OrderController::class,"update"]);
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::patch("updateVenue/{id}",[VenueController::class,"update"]);
