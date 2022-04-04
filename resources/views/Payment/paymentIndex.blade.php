f8d631f944d0b1f57cf2ca4abbba557db8fd30ea
GeorgeXr <kevinlarry0927@gmail.com>
2022-04-04 17:46:37 +0800

    added paymentIndex

---
+++ resources/views/Payment/paymentIndex.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Payment Index Page</title>
+		<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	</head>
+	<body>
+		<div class="container">
+		<br />
+		@if (\Session::has('success'))
+			<div class="alert alert-success">
+				<p>{{\Session::get('success') }}</p>
+			</div> <br />
+			@endif
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Payment ID</th>
+						<th>Delivery Order ID</th>
+						<th>Total Amount</th>
+                                                <th>Payment Type</th>
+                                                <th>Status</th>
+						<th colspan="2">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($payments as $payment)
+					<tr>
+						<td>{{$payment['paymentID']}}</td>
+						<td>{{$payment['deliveryOrderID']}}</td>
+						<td>{{$payment['totalAmount']}}</td>
+                                                <td>{{$payment['paymentType']}}</td>
+                                                <td>{{$payment['status']}}</td>
+						
+						<td>
+							<a href="{{action('App\Http\Controllers\PaymentController@edit', $payment['paymentID'])}}" class="btn btn-warning">Edit</a>
+						</td>
+						<td>
+							<form action="{{action('App\Http\Controllers\PaymentController@destroy', $payment['paymentID'])}}" method="post">
+							@csrf
+							<input name="_method" type="hidden" value="DELETE">
+							<button class="btn btn-danger" type="submit">Delete</button>
+							</form>
+						</td>
+					</tr>
+					@endforeach	
+				</tbody>
+			</table>
+		</div>
+	</body>
+</html>
